882cfb0f5a3fbb7ba68eb6d158b3af843c0aa5a0
akai1881 <akaibegaliev1881@gmail.com>
2022-04-27 06:36:02 +0600

    svg map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import RadioSVGMap from "./RadioSvg";
+import KgMap from "./map";
+import "./App.css";
+import { useState } from "react";
+
+const options = [
+    { id: "issyk", data: [{ street: "test issyk street", phone: "test issyk phone" }] },
+    { id: "osh", data: [{ street: "test osh street", phone: "test osh phone" }] },
+    { id: "chui", data: [{ street: "test issyk street", phone: "test issyk phone" }] },
+    { id: "talas", data: [{ street: "test osh street", phone: "test osh phone" }] },
+    { id: "jalal-abad", data: [{ street: "test issyk street", phone: "test issyk phone" }] },
+    { id: "batken", data: [{ street: "test osh street", phone: "test osh phone" }] },
+    { id: "naryn", data: [{ street: "test osh street", phone: "test osh phone" }] }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [focusedLocation, setFocusedLocation] = useState(null);
+
+    const [selectRegion, setSelectedRegion] = useState(options[0]);
+
+    const [modal, setModal] = useState(false);
+
+    const getLocationName = event => {
+        return event.target.attributes.name.value;
+    };
+
+    const handleFocus = (event, location) => {
+        const focusedLocation = getLocationName(event);
+        setSelectedRegion(options.find(item => focusedLocation === item.id));
+        setModal(true);
+        setFocusedLocation(focusedLocation);
+    };
+
+    const getLocationClassName = (location, index) => {
+        return `svg-map__location svg-map__location--heat${index % 14}`;
+    };
+
+    return (
+        <>
+            <div className="kg-map">
+                <RadioSVGMap map={KgMap} locationClassName={getLocationClassName} onLocationFocus={handleFocus} />
+            </div>
+            {modal && (
+                <div>
+                    {selectRegion.id}, <button onClick={() => setModal(false)}>close</button>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
---
+++ src/Svg.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function SVGMap(props) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={props.map.viewBox}
+            className={props.className}
+            role={props.role}
+            aria-label={props.map.label}
+        >
+            {props.childrenBefore}
+            {props.map.locations.map((location, index) => {
+                return (
+                    <path
+                        id={location.id}
+                        name={location.name}
+                        d={location.path}
+                        className={
+                            typeof props.locationClassName === "function"
+                                ? props.locationClassName(location, index)
+                                : props.locationClassName
+                        }
+                        tabIndex={
+                            typeof props.locationTabIndex === "function"
+                                ? props.locationTabIndex(location, index)
+                                : props.locationTabIndex
+                        }
+                        role={props.locationRole}
+                        aria-label={
+                            typeof props.locationAriaLabel === "function"
+                                ? props.locationAriaLabel(location, index)
+                                : location.name
+                        }
+                        aria-checked={props.isLocationSelected && props.isLocationSelected(location, index)}
+                        onMouseOver={props.onLocationMouseOver}
+                        onMouseOut={props.onLocationMouseOut}
+                        onMouseMove={props.onLocationMouseMove}
+                        onClick={props.onLocationClick}
+                        // onKeyDown={props.onLocationKeyDown}
+                        onFocus={e => props.onLocationFocus(e, location)}
+                        onBlur={props.onLocationBlur}
+                        key={location.id}
+                    />
+                );
+            })}
+            {props.childrenAfter}
+        </svg>
+    );
+}
+
+export default SVGMap;
